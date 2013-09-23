1ac3bc962e277fcc1afab32f9695dc7157ee53f4
Justin Fletcher <justin@justinandco.com>
2013-09-23 10:59:16 +0100

    added removal of the capabilities

--- uninstall.php
+++ uninstall.php
@@ -10,12 +10,54 @@ if (is_multisite()) {
     if ($blogs) {
         foreach($blogs as $blog) {
             switch_to_blog($blog['blog_id']);
+            do_action( 'rbhn_remove_caps');  // clear out capabilities
             delete_option('help_note_option');
         }
         restore_current_blog();
     }
 } else {
+    do_action( 'rbhn_remove_caps');  // clear out capabilities
     delete_option('help_note_option');
 }
+
+
+// remove capabilities on uninstall.
+function rbhn_remove_caps() {
+
+	global $wp_roles;
+	
+	if ( ! isset( $wp_roles ) )
+	$wp_roles = new WP_Roles();
+
+	$roles = $wp_roles->get_names();
+	
+	//ksort($roles);
+	
+	foreach($roles as $role_key=>$role_name)
+	{
+		// gets the author role
+		$role = get_role( $role_key );
+		$capability_type = "help_{$role_key}_note";
+		
+		//http://stackoverflow.com/questions/8198038/wordpress-capabilities-for-custom-post-type
+		$role->remove_cap( "edit_{$capability_type}" );
+		$role->remove_cap( "read_{$capability_type}" );
+		$role->remove_cap( "delete_{$capability_type}" );
+		$role->remove_cap( "edit_{$capability_type}s" );
+		$role->remove_cap( "edit_others_{$capability_type}s" );
+		$role->remove_cap( "publish_{$capability_type}s" );
+		$role->remove_cap( "read_private_{$capability_type}s" );
+        $role->remove_cap( "delete_{$capability_type}s" );
+        $role->remove_cap( "delete_private_{$capability_type}s" );
+        $role->remove_cap( "delete_published_{$capability_type}s" );
+        $role->remove_cap( "delete_others_{$capability_type}s" );
+        $role->remove_cap( "edit_private_{$capability_type}s" );
+        $role->remove_cap( "edit_published_{$capability_type}s" );
+				
+	}
+
+}
+
+
 
 ?>
