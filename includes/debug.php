ee000e0d0220392b8a00e500252cb8f7460a0e7c
Justin Fletcher <justin@justinandco.com>
2013-09-23 14:14:20 +0100

    tweaked to put debug dumped variable contents into a help tab of the admin side.

--- includes/debug.php
+++ includes/debug.php
@@ -2,64 +2,76 @@
 
 
 
-add_action('admin_init', 'my_debug', 12 );      // add to admin
-if (!is_admin()) {
-	add_action('init', 'my_debug', 12 );    	// add to frontend and hook after helpnotes has executed.
-}	
+
+
+add_action( 'contextual_help', 'rbhn_screen_help', 10, 3 );
+function rbhn_screen_help( $contextual_help, $screen_id, $screen ) {
+ 
+    // The add_help_tab function for screen was introduced in WordPress 3.3.
+    if ( ! method_exists( $screen, 'add_help_tab' ) )
+        return $contextual_help;
+ 
+ 
+    // Append global $hook_suffix to the hook stems
+    $hooks = array(
+        "load-$hook_suffix",
+        "admin_print_styles-$hook_suffix",
+        "admin_print_scripts-$hook_suffix",
+        "admin_head-$hook_suffix",
+        "admin_footer-$hook_suffix"
+    );
+ 
+    $help_content = my_debug();
+ 
+    // Add help panel
+    $screen->add_help_tab( array(
+        'id'      => 'role-based-help-notes-help',
+        'title'   => 'Capabilty Debug Information',
+        'content' => $help_content,
+    ));
+ 
+    return $contextual_help;
+}
+
+
+
 
 function my_debug(){
 
-
-	//print_r ('h_general....<br />');
-	//print_r( get_post_type_object('h_general') );
-
-	
-	print '</pre>';
 	//Tons of information about the   review    post type (change review to your post type)
 	//Paste anywhere on a theme template file outside the loop.
 
-
+    
 	global $wp_post_types;
-	print '<pre>';
 	
-	print_r ('$wp_post_types....<br />');
-	
-	print_r( $wp_post_types );
-	print '</pre>';
+    $debug_text = "<br /><br /><h3>Post Types</h3>";
+    $debug_text .= "<br /> <br />";
+    $debug_text .= "<pre>" . $wp_post_types ."</pre>" ;
+    //$debug_text .=  '<li>' . implode( '</li><li>', $wp_post_types ) . '</li>' ;
+
+
 	// Tons of information about all the different post types on the site
 	//Paste anywhere on a theme template file outside the loop.
-
-
-	echo '<br /><br /><h3>Roles</h3>';
-	
+    $debug_text .= "<br /><br /><h3>Roles</h3>";
 	global $wp_roles;
 	
-	//if ( ! isset( $wp_roles ) )
-	//$wp_roles = new WP_Roles();
 
-	
-	//echo var_dump($wp_roles->role_names );
-		foreach ( $wp_roles->role_names as $role => $name ) :
-			echo '<br /> <br />';
-			echo '<pre> Role displayed in Admin as ' . $name ;
-			echo  '     Database entry: '  . $role . '</pre>';
+	foreach ( $wp_roles->role_names as $role => $name ) :
+		$debug_text .= "<br /> <br />";
+		$debug_text .= "<pre> Role displayed in Admin as " . $name ;
+		$debug_text .= "     Database entry: "  . $role . "</pre>";
 
-			echo '<h5> Capabilities assigned to the role of ' . $name. '</h5>';
-			// print_r( $caps);
-			echo '<pre>';
-			$rolename = get_role($role);
-			$caps = $rolename->capabilities;
-				foreach ($caps as $capability => $value):
-					echo  $capability . ' '.  $value . "\n" ;
-				endforeach;
-			echo '</pre>';
-		endforeach;
+		$debug_text .= "<h5> Capabilities assigned to the role of " . $name. "</h5>";
+		$debug_text .= "<pre>";
+		$rolename = get_role($role);
+		$caps = $rolename->capabilities;
+			foreach ($caps as $capability => $value):
+				$debug_text .= "$capability  $value\n" ;
+			endforeach;
+		$debug_text .= "</pre>";
+	endforeach;
 
-	//Show the role display name and the name used in the database for each role on the site.
-	//Show the capabilities assigned to that role
-	//Paste anywhere on a theme template file outside the loop.
-	//Thanks to Greenshady: http://wordpress.org/support/topic/get-a-users-role-by-user-id
-	// Thanks to:  http://sillybean.net/wordpress/creating-roles/
+    return $debug_text;
 
 }
 
