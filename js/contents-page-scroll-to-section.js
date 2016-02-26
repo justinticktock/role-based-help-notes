9c90f4612df2c53a9111c4cc225e83c026e959eb
Justin Fletcher <justinandco.com>
2016-02-26 16:53:16 +0000

    update contents-page-scroll-to-section.js

--- js/contents-page-scroll-to-section.js
+++ js/contents-page-scroll-to-section.js
@@ -31,27 +31,79 @@
     })
     
     
-  $('.dashicons-edit').hide();
-    
-    $(function() {
+ // $('.dashicons-edit').hide();
+ // $('.dashicons-edit').show();  
+ //   $(function() {
         
-    //$('.rbhn-link').hover(function() { 
-    $("*[class*='page-item-']").hover(function() { 
+  //  $('rbhn-link').hover(function() {
+   // $("*[class*='page-item-']").hover(function() { 
+   //  $('.dashicons-edit').show();
+    $("*[class*='rbhn-link']").hover(function() { 
+
+        
+var current_class = $(this).attr('class'); 
+//console.log(current_class);   
+
+
      // $(this).closest('.dashicons-edit').fadeIn(); 
        //$('.dashicons-edit').stop(true, true).fadeIn(); 
-      $(this).find('.dashicons-edit').stop(true, true).fadeIn(); 
-      
+
+   //   $('a.rbhn-link').prevAll('a.dashicons-edit:first').fadeIn(); 
+        
       // $(this).parent('.dashicons-edit').appendTo(this);
       // $(this).closest('.dashicons-edit').appendTo(this);
        //$(this).find('.dashicons-edit').appendTo(this);
-       $(this).find('.dashicons-edit').appendTo(this);
+      // $(this).find('.dashicons-edit').appendTo($(this).find('.rbhn-link:first'));
+      // $(this).find('.dashicons-edit').stop(true, true).fadeIn(); 
+     // $(this).find('.dashicons-edit').stop(true, true).fadeIn(); 
+
+     // $(this).find('.dashicons-edit').stop(true, true).fadeIn(); 
+
+ $(this).siblings('.dashicons-edit').fadeIn('fast'); 
+
+//$('a.rbhn-link').prevAll('a.dashicons-edit:first').fadeIn(); 
+    //   exit();
+//var edit_icon = $('a.rbhn-link').prevAll('a.dashicons-edit:first').detach();
+
+//$('a.rbhn-link').prevAll('a.dashicons-edit:first').fadeIn(); 
+//$(this).append(edit_icon);
+
+
+
+var previous_edit_icon =  $('li.rbhn-link').prevAll('li.dashicons-edit:first').attr('class'); 
+//console.log(previous_edit_icon);    
+var this_link_name =  $(this).attr('class'); 
+//console.log(this_link_name);  
+
+//http://stackoverflow.com/questions/2310270/jquery-find-closest-previous-sibling-with-class
+//$('a.rbhn-link').prevAll('a.dashicons-edit:first').appendTo(this);
+   //   $(this).prepend($('a.rbhn-link').prevAll('a.dashicons-edit:first'));
        
-       
+
     }, function() { 
        // $(this).closest('.dashicons-edit').fadeOut(); 
        // $('.dashicons-edit').stop(true, true).fadeOut(); 
-      $(this).find('.dashicons-edit').stop(true, true).fadeOut(); 
-    });
+    
+     // $(this).find('.dashicons-edit').remove($(this).find('.rbhn-link'));
+     
+    // $(this).siblings('.dashicons-edit').stop(true, true).fadeOut(); 
+    
+  //  $('li.dashicons-edit').prevAll('li.rbhn-link:first').appendTo(this);
+    
+//$('.dashicons-edit').hover(function() { 
+
+$('.dashicons-edit').not(".Class").hover(function() { 
+
+var this_link_name =  $(this).attr('class'); 
+console.log(this_link_name);  
+//$(this).siblings('.dashicons-edit').stop(true, true).fadeIn(); 
+
+}, function() { 
+
+//$(this).siblings('.dashicons-edit').stop(true, true).fadeOut(); 
+$('.dashicons-edit').fadeOut(); 
+});
+
     });
 
 
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: justinticktock
 Tags: multisite, roles, user, help, notes, cms, documents, groups, teams, collaboration, BuddyPress, intranet
 Requires at least: 3.5
-Tested up to: 4.3
+Tested up to: 4.4
 Stable tag: 1.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
