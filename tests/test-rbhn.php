a309273c66925ca83366bec1ebaeddfd4f6cfcbd
Justin Fletcher <justinandco.com>
2016-09-18 21:52:14 +0100

    basic test

---
+++ tests/test-rbhn.php
@@ -0,0 +1,15 @@
+<?php
+
+class rbhn_test extends PHPUnit_Framework_TestCase {
+        public function setUp() {
+        // This code will run before each test!
+        }
+
+        public function tearDown() {
+        // This code will run after each test
+        }
+
+        public function testBasic() {
+        $this->assertTrue( true );
+        }
+}
